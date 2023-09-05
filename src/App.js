fcd135ce130b94641c63dc907e485f2d6789fc7b
이지혁 <illu140411@gmail.com>
2023-09-05 15:31:28 +0900

    init: Route 추가

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      {/* <Navbar></Navbar> */}
+      {/* <RouterLink></RouterLink> */}
+    </div>
+  );
 }
 
 export default App;
