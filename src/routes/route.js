93ec960edb4b67bb96fe4035bcdbb520bbb88cc4
lIIIlllIII <90883458+mko56@users.noreply.github.com>
2023-09-19 01:41:32 +0900

    feat : router수정

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,16 +1,17 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes } from "react-router-dom";
 
-import MainPage from '../pages/MainPage/MainPage.jsx';
-import GuidePage from '../pages/GuidePage/GuidePage.jsx';
-import GuideDetailPage from '../pages/GuideDetailPage/GuideDetailPage.jsx';
-import AreaPage from '../pages/AreaPage/AreaPage.jsx';
-import Mypage from '../pages/MyPage/Mypage.jsx';
-
+import MainPage from "../pages/MainPage/MainPage.jsx";
+import GuidePage from "../pages/GuidePage/GuidePage.jsx";
+import GuideDetailPage from "../pages/GuideDetailPage/GuideDetailPage.jsx";
+import AreaPage from "../pages/AreaPage/AreaPage.jsx";
+import Mypage from "../pages/MyPage/Mypage.jsx";
+import Login from "../pages/LoginPage/Login.jsx";
+import Signup from "../pages/LoginPage/Signup.jsx";
 // WantTour
-import WantTourList from '../pages/WantTour/WantTourList.jsx';
-import WantTourDetail from '../pages/WantTour/WantTourDetail.jsx';
-import WantTourRegist from '../pages/WantTour/WantTourRegist.jsx';
-import WantTourUpdate from '../pages/WantTour/WantTourUpdate.jsx';
+import WantTourList from "../pages/WantTour/WantTourList.jsx";
+import WantTourDetail from "../pages/WantTour/WantTourDetail.jsx";
+import WantTourRegist from "../pages/WantTour/WantTourRegist.jsx";
+import WantTourUpdate from "../pages/WantTour/WantTourUpdate.jsx";
 
 function RouteLink() {
   return (
@@ -18,8 +19,8 @@ function RouteLink() {
       <Routes>
         <Route path="/" element={<MainPage />} />
         {/* <Route path="/products" element={<Themeproduct />} /> */}
-        {/* <Route path="/login" element={<Login />} /> */}
-        {/* <Route path="/signup" element={<Signup />} /> */}
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
         {/* <Route path="/create" element={<Createproduct />} /> */}
         <Route path="/area" element={<AreaPage />} />
         <Route path="/guide" element={<GuidePage />} />
