9c3fdef1ed2442c301e1913d3e7ad6a10603b1f0
daehyuk <sdh5645@gmail.com>
2023-10-16 14:00:52 +0900

    feat: login

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,34 +1,34 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes } from "react-router-dom";
 
-import MainPage from 'pages/MainPage/MainPage.jsx';
-import GuidePage from 'pages/GuidePage/GuidePage.jsx';
-import GuideDetailPage from 'pages/GuideDetailPage/GuideDetailPage.jsx';
-import AreaPage from 'pages/AreaPage/AreaPage.jsx';
-import Mypage from 'pages/MyPage/Mypage.jsx';
-import Login from 'pages/LoginPage/Login.jsx';
-import Signup from 'pages/LoginPage/Signup.jsx';
+import MainPage from "pages/MainPage/MainPage.jsx";
+import GuidePage from "pages/GuidePage/GuidePage.jsx";
+import GuideDetailPage from "pages/GuideDetailPage/GuideDetailPage.jsx";
+import AreaPage from "pages/AreaPage/AreaPage.jsx";
+import Mypage from "pages/MyPage/Mypage.jsx";
+import Login from "pages/LoginPage/Login.jsx";
+import Signup from "pages/LoginPage/Signup.jsx";
 // WantTour
-import WantTourList from 'pages/WantTour/WantTourList.jsx';
-import WantTourDetail from 'pages/WantTour/WantTourDetail.jsx';
-import WantTourRegist from 'pages/WantTour/WantTourRegist.jsx';
-import WantTourUpdate from 'pages/WantTour/WantTourUpdate.jsx';
+import WantTourList from "pages/WantTour/WantTourList.jsx";
+import WantTourDetail from "pages/WantTour/WantTourDetail.jsx";
+import WantTourRegist from "pages/WantTour/WantTourRegist.jsx";
+import WantTourUpdate from "pages/WantTour/WantTourUpdate.jsx";
 
 // Recommend
-import FestivalList from 'pages/RecommendPage/FestivalList.jsx';
-import FestivalDetail from 'pages/RecommendPage/FestivalDetail.jsx';
-import LocationList from 'pages/RecommendPage/LocationList.jsx';
-import LocationDetail from 'pages/RecommendPage/LocationDetail.jsx';
-import TastyList from 'pages/RecommendPage/TastyList.jsx';
-import TastyDetail from 'pages/RecommendPage/TastyDetail.jsx';
+import FestivalList from "pages/RecommendPage/FestivalList.jsx";
+import FestivalDetail from "pages/RecommendPage/FestivalDetail.jsx";
+import LocationList from "pages/RecommendPage/LocationList.jsx";
+import LocationDetail from "pages/RecommendPage/LocationDetail.jsx";
+import TastyList from "pages/RecommendPage/TastyList.jsx";
+import TastyDetail from "pages/RecommendPage/TastyDetail.jsx";
 
 // Tour
-import TourList from 'pages/Tour/TourList.jsx';
-import TourRegist from 'pages/Tour/TourRegist.jsx';
-import TourDetailPage from 'pages/Tour/TourDetail.jsx';
+import TourList from "pages/Tour/TourList.jsx";
+import TourRegist from "pages/Tour/TourRegist.jsx";
+import TourDetailPage from "pages/Tour/TourDetail.jsx";
 
 //router
-import PrivateRouter from './PrivateRouter';
-import PublicRouter from './PublicRouter';
+import PrivateRouter from "./PrivateRouter";
+import PublicRouter from "./PublicRouter";
 
 function RouteLink() {
   return (
@@ -36,8 +36,7 @@ function RouteLink() {
       <Routes>
         <Route path="/" element={<MainPage />} />
         {/* <Route path="/products" element={<Themeproduct />} /> */}
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
+
         {/* <Route path="/create" element={<Createproduct />} /> */}
         <Route path="/area" element={<AreaPage />} />
         <Route path="/guide" element={<GuidePage />} />
@@ -57,13 +56,18 @@ function RouteLink() {
         <Route path="/tour/tourdetail/:id" element={<TourDetailPage />} />
         {/* <Route path="/resetpassword" element={<ResetPassword />} /> */}
         {/* <Route path="/tour/:id" element={<ProductDetail />} /> */}
-        {/* <PrivateRouter>
+        <Route Component={PublicRouter}>
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Route>
+
+        <Route Component={PrivateRouter}>
           <Route path="/mypage/:id" element={<Mypage />} />
           <Route path="/tour/tourregist" element={<TourRegist />} />
           <Route path="/wanttour/regist" element={<WantTourRegist />} />
           <Route path="/wanttour/detail/:id" element={<WantTourDetail />} />
-         <Route path="/wanttour/update/:id" element={<WantTourUpdate />} />
-        </PrivateRouter> */}
+          <Route path="/wanttour/update/:id" element={<WantTourUpdate />} />
+        </Route>
       </Routes>
     </>
   );
