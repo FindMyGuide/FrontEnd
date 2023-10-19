49dee2c61cd5acce5c94be2024fd1ebb0bdffa5c
송아람 <0807com@naver.com>
2023-10-19 21:03:18 +0900

    feat: main 수정

--- src/components/Card/GuideCard.jsx
+++ src/components/Card/GuideCard.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import noProfile from 'asset/icons/noProfile.svg';
 import styles from './GuideCard.module.css';
 
 function GuideCard({ guide }) {
+  const navigate = useNavigate();
   console.log(guide, 'guideCard에서 확인');
+  
+  const onClickHandler = (id) => {
+    navigate(`/guide/detail/${id}`);
+  };
 
   return (
-    <div className={styles.container}>
+    <div className={styles.container} onClick={() => onClickHandler(guide.guideId)}>
       <div className={styles.guideTop}>
         <div className={styles.Name}>{guide.guideName}</div>
         {guide.profilePicture ? (
@@ -15,8 +21,14 @@ function GuideCard({ guide }) {
           <img src={noProfile} alt="guideImg" className={styles.profileImg}></img>
         )}
       </div>
-      {guide.hasGuideCertification ? <span className={styles.guideCertificate}>가이드자격증</span> : null}
-      <div>{guide.guideEmail}</div>
+      <div>
+        {guide.hasGuideCertification ? (
+          <span className={styles.guideCertificate}>가이드 자격증</span>
+        ) : (
+          <div>&nbsp;</div>
+        )}
+      </div>
+      <div className={styles.explain}>{guide.guideEmail}</div>
     </div>
   );
 }
